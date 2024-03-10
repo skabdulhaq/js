e05a0628723f9f2c1fc2d46b223cbd0df7548ad1
Abdul Haq <75989562+skabdulhaq@users.noreply.github.com>
2024-03-10 12:06:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 const urlParams = new URLSearchParams(window.location.search);
-        const storedName = urlParams.get('name');
-        if (storedName) {
-            document.querySelector('.message').innerHTML = 'Welcome, '+storedName;
-        }
-        const storedSecret = localStorage.getItem('secret');
-        if (storedSecret) {
-            document.querySelector('.message').innerHTML += '<br>Your stored secret is: ' + storedSecret;
-        }
-        document.getElementById('secret-form').addEventListener('submit', function(event) {
-            event.preventDefault();
-            const name = document.getElementById('name').value;
-            const secret = document.getElementById('secret').value;
-            const params = new URLSearchParams(window.location.search);
-            params.set('name', name);
-            history.replaceState({}, '', `${location.pathname}?${params.toString()}`);
-            localStorage.setItem('secret', secret);
-            document.querySelector('.message').innerHTML = `Welcome, ${name}! Your secret has been stored! Reload to see your secret.`;
-                var messageDiv = document.querySelector('.message');
-                var scriptTags = messageDiv.querySelectorAll('script');
-                scriptTags.forEach(function(script) {
-                script.parentNode.removeChild(script);
-        });
-        });
+const storedName = urlParams.get('name');
+if (storedName) {
+    document.querySelector('.message').innerHTML = 'Welcome, '+storedName;
+}
+const storedSecret = localStorage.getItem('secret');
+if (storedSecret) {
+    document.querySelector('.message').innerHTML += '<br>Your stored secret is: ' + storedSecret;
+}
+document.getElementById('secret-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+    const secret = document.getElementById('secret').value;
+    const params = new URLSearchParams(window.location.search);
+    params.set('name', name);
+    history.replaceState({}, '', `${location.pathname}?${params.toString()}`);
+    localStorage.setItem('secret', secret);
+    document.querySelector('.message').innerHTML = `Welcome, ${name}! Your secret has been stored! Reload to see your secret.`;
+});
+var messageDiv = document.querySelector('.message');
+var scriptTags = messageDiv.querySelectorAll('script');
+scriptTags.forEach(function(script) {
+script.parentNode.removeChild(script);
+});
